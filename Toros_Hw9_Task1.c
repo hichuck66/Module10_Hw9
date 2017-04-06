0a7fdb81458c0a03895ee2fcf872bf29bff8bdd4
ChikereNjoku Chibuike <cchikerenjoku@weber.edu>
2017-04-06 16:07:42 +0000

    first commit

---
+++ Toros_Hw9_Task1.c
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Toros_Hw9_Task1.c
+ *       	Usage:  ./Toros_Hw9_Task1.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/06/2017 04:06:49 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Chikere-Njoku Chibuike (), cchikerenjoku@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O*/
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+
+	return 0;
+}
+
+
+/* Function Definitions */
+
+
