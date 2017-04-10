aa0660dbe8c2b24bf4eb50d150a0d7cad749e382
Tanner Nelson <tannernelson3@mail.weber.edu>
2017-04-10 03:58:14 +0000

    Task1 first commit

--- Toros_Hw9_Task1.c
+++ Toros_Hw9_Task1.c
@@ -15,11 +15,27 @@
 #include <stdlib.h>
 
 /* Function Prototypes */
+void Usage(char **info);
+void ReadFile(char *fName, float num[]);
+FILE *OpenCheckFile(char *fName);
+void WriteFile(FILE *sFile, float num[]);
 
 /* Main Program */
 int main(int argc, char *argv[])
 {
+	char *fName;
+	float num[];
+	argv[1] = fName;
 
+	if(argc != 2||argv[1] = "--help")
+	{
+		Usage();
+	}
+	else
+	{
+		ReadFile(fName,num);
+		OpenCheckFile(fName);
+	}
 
 	return 0;
 }
@@ -27,4 +43,56 @@ int main(int argc, char *argv[])
 
 /* Function Definitions */
 
+void Usage()
+{
+printf("Usage ./hw9_task1 <dataFile> <outFileName>");
+return;
+}
 
+int ReadFile(char *fName, float num[])
+{
+	FILE *fName = fopen("inNumbers.dat","r");
+	
+	float num;
+	int i = 0;
+	int n;
+
+	while(fscanf(fName, "%d", &num) > 0)
+	{
+		num[i] = n;
+		i++;
+	}
+	fclose(fName);
+
+return 0;
+
+}
+
+FILE *OpenCheckFile(char *fName)
+{
+	if(access(fopen(fName, F_OK) !=-1)
+	{
+		char i;
+		printf("file exists. Do you want to overwrite it? Y/N");
+		scanf("%s", i);
+		if(i = "Y"||i = "y")
+		{
+			WriteFile();
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	else
+	{
+		printf("File does not yet exist.");
+		WriteFile();
+	}
+	return 0;
+}
+
+void WriteFile(FILE *sFile, float num[]);
+{
+	FILE *sFile = fprint
+}
